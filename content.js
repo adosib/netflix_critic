fee39cb7c90eaa506ae6d9a8c01a7800790db3aa
adosib <adosib1@gmail.com>
2025-01-08 15:42:34 -0600

    Refactoring and clean-up

--- content.js
+++ content.js
@@ -57,65 +57,19 @@ async function getData(url){
     }
 }
 
-async function extractContextVarFromTitlePage(netflixId) {
-
-    const url = `https://www.netflix.com/title/${netflixId}`;
-    const response = await fetch(url, {method: 'GET', credentials: 'omit',});
-    if (!response.ok) {
-      throw new Error(`Request for ${url} failed with status ${response.status}`);
-    }
-    const html = await response.text();
-
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    
-    const scriptTags = doc.querySelectorAll('script');
-    let scriptContent = null;
-  
-    scriptTags.forEach(script => {
-      if (script.textContent.includes('netflix.reactContext')) {
-        scriptContent = script.textContent.replaceAll('netflix.reactContext', `netflix.reactContext${netflixId}`);
-      }
-    });
-  
-    if (!scriptContent) {
-      throw new Error(`Could not find script with netflix.reactContext for title with ID ${netflixId}`);
-    }
-  
-    const scriptId = `netflix-critic-script-${netflixId}`;
-    // Create a temporary script element to evaluate the script content
-    if(!document.scripts.namedItem(scriptId)){
-        const script = document.createElement('script');
-        script.classList.add('netflix-critic');
-        script.id = scriptId;
-        script.textContent = scriptContent;
-        document.body.appendChild(script);
-    }
-
-    const contextVar = window.netflix[`reactContext${netflixId}`];
-    console.log(contextVar);
-
-    setTimeout(() => {
-        document.body.removeChild(document.scripts.namedItem(scriptId));
-    }, 2000);
-    
-    return contextVar.models.nmTitleUI.data.sectionData;
-}
-
 class TitleCard {
+    // Private cache
+    #netflixId = null;
+
     constructor(divElement){
         // Check if the passed element is a valid DOM node and has the expected structure
         if (!(divElement instanceof HTMLDivElement)){
             throw new Error("Expected an HTMLDivElement.");
         }
         this.divElement = divElement;
-        this.data = this.fetchData();
         divElement.dataset.netflixCriticProcessed = true;
         divElement.netflixCriticTitleCardObj = this;
     }
-
-    // Private cache
-    #netflixId = null;
 
     get netflixId() {
         if (this.#netflixId) {
@@ -125,17 +79,14 @@ class TitleCard {
         const watchLink = this.divElement.querySelector('a').href;
         const splitPath = URL.parse(watchLink).pathname.split('/');
         const parsedId = parseInt(splitPath[splitPath.length - 1]);
+
         this.#netflixId = parsedId;  // Cache the result
+
         return parsedId;
     }
 
     get title(){
         return this.divElement.getElementsByClassName("fallback-text")[0].innerText;
-    }
-
-    async fetchData(){
-        const data = await extractContextVarFromTitlePage(this.netflixId);
-        return data;
     }
 
     async getProperty(property) {
@@ -156,13 +107,8 @@ class TitleCard {
     }
     
     get runtime() {
-        //return this.getProperty("runtime");
-        const runtimeSeconds = this.data.then((data) => { data[0].data.details[0].data.runtime; });
-        // const runtimeSeconds = data[0].data.details[0].data.runtime;
-        // return new Date(runtimeSeconds * 1000).toISOString().substring(11, 16);
-        return runtimeSeconds;
+        return this.getProperty("runtime");
     }
-
 
     #storeData(jsonData){
         localStorage.setItem(jsonData["netflix_id"], JSON.stringify(jsonData));
@@ -185,10 +131,6 @@ class TitleCard {
 }
 
 function reloadDOM(){
-    let titleCardsSet = new Set();  // to hold unique movie titles
-    let titleCardsDict = {};  // to hold movie-rating pairs
-    let backgroundMessage = ["url", {}]; // will store 2 elements, the URL and the movies dict
-
     // TODO I probably want to narrow this selector down to elements I haven't processed already, if possible
     let titleCardsArr = document.querySelectorAll('.title-card-container:not([data-netflix-critic-processed="true"])');
     for(i = 0; i < titleCardsArr.length; i++){
@@ -196,18 +138,6 @@ function reloadDOM(){
         // TODO handle all processing in the constructor
         title.divElement.innerHTML += "<div class=netflix-critic><p>" + title.runtime + "</p></div>";
     }
-    // TODO vvv clean up this crap
-    // backgroundMessage[0] = window.location.toString();
-    // backgroundMessage[1] = titleCardsDict;
-    // console.log(titleCardsSet);
-    // console.log(titleCardsSet.size);
-    // console.log(backgroundMessage);
-    // callbackFunc(backgroundMessage);
-}
-
-function getRating(){
-    let rating = "NA";
-    return rating;
 }
 
 function callbackFunc(backgroundMessage){
