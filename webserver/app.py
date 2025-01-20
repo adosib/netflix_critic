9d8d53c42fbe635f85751b1fb4d1934bcb2733f0
adosib <adosib1@gmail.com>
2025-01-20 10:41:38 -0600

    Refactoring - moved a lot of stuff to common.py module in the git submodule

--- webserver/app.py
+++ webserver/app.py
@@ -1,7 +1,5 @@
-import sys
 import json
 import asyncio
-import logging
 import itertools
 from http import HTTPStatus
 from uuid import uuid4
@@ -9,8 +7,7 @@ from typing import Any, Dict, Optional, Annotated
 from pathlib import Path
 from collections import defaultdict
 
-import pythonmonkey as pm
-from bs4 import BeautifulSoup
+import app_logger
 from models import Title, Rating, Availability
 from fastapi import (
     Query,
@@ -30,65 +27,33 @@ from fastapi.staticfiles import StaticFiles
 from starlette.background import BackgroundTask
 from starlette.middleware.cors import CORSMiddleware
 
-from netflix_critic_data.scripts.database_setup.backfill_titles import (
+from netflix_critic_data.scripts.database_setup.common import (
+    JobStore,
     HTMLContent,
+    HttpSessionHandler,
+    ContextExtractionError,
     get_field,
-)
-from netflix_critic_data.scripts.database_setup.populate_ratings import (
     get_ratings,
     get_serp_html,
-)
-from netflix_critic_data.scripts.database_setup.populate_availability import (
-    SessionHandler,
+    configure_logger,
     save_response_body,
+    extract_netflix_react_context,
 )
 
 THIS_DIR = Path(__file__).parent
 ROOT_DIR, *_ = [
     parent for parent in THIS_DIR.parents if parent.stem == "netflix_critic"
 ]
-DOWNLOADED_TITLEPAGES_DIR = ROOT_DIR / "netflix_critic_data" / "data" / "raw" / "title"
-DATABASE_URL = "postgresql://localhost:5432/postgres"
+DOWNLOADED_TITLEPAGES_DIR = ROOT_DIR / "data" / "raw" / "title"  # TODO
+DOWNLOADED_SERP_PAGES_DIR = ROOT_DIR / "data" / "raw" / "serp"  # TODO
+DATABASE_URL = "postgresql+psycopg://localhost:5432/postgres"
 STATUS_REASONS = {x.value: x.name for x in list(HTTPStatus)}
 
 APIFY_SEMAPHORE = asyncio.Semaphore(32)
 
-
-class JobStore:
-    """Data structure to avoid re-processing title IDs that have been processed already."""
-
-    def __init__(self):
-        self._data = defaultdict(list)
-        self._global_values = set()  # Set to enforce global uniqueness of values
-
-    def add(self, key: Any, values: list):
-        """Add a value to a key. Ensure global uniqueness."""
-        if key not in self._data:
-            self._data[key]
-
-        for value in values:
-            if value in self._global_values:
-                continue
-
-            self._data[key].append(value)
-            self._global_values.add(value)
-
-    def get(self, key):
-        """Get all values associated with a key."""
-        return self._data.get(key, [])
-
-    def __setitem__(self, key, value):
-        return self.add(key, value)
-
-    def __getitem__(self, key):
-        return self.get(key)
-
-    def __repr__(self):
-        return repr(self._data)
-
-
 global_job_store = JobStore()
 
+engine = create_engine(DATABASE_URL, echo=True)
 
 app = FastAPI()
 app.mount("/title", StaticFiles(directory=DOWNLOADED_TITLEPAGES_DIR, html=True))
@@ -98,9 +63,9 @@ templates = Jinja2Templates(directory=THIS_DIR / "templates")
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=False,
-    allow_origins=["*"],  # Or more restricted origins like "https://www.netflix.com"
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
     expose_headers=[
         "X-Requested-With",
         "X-HTTP-Method-Override",
@@ -113,67 +78,20 @@ app.add_middleware(
 )
 
 
-######################## https://stackoverflow.com/questions/70891687/how-do-i-get-my-fastapi-applications-console-log-in-json-format-with-a-differen/70899261#70899261
-class CustomJSONFormatter(logging.Formatter):
-    def __init__(self, fmt):
-        super().__init__(fmt)
-
-    def format(self, record):
-        super().format(record)
-        return json.dumps(get_log(record), indent=2)
-
-
-def get_log(record):
-    d = {
-        "time": record.asctime,
-        "process_name": record.processName,
-        "process_id": record.process,
-        "thread_name": record.threadName,
-        "thread_id": record.thread,
-        "level": record.levelname,
-        "logger_name": record.name,
-        "pathname": record.pathname,
-        "line": record.lineno,
-        "message": record.message,
-    }
-
-    if hasattr(record, "extra_info"):
-        d["req"] = record.extra_info["req"]
-        d["res"] = record.extra_info["res"]
-
-    return d
-
-
-def get_file_handler(
-    formatter, filename=(ROOT_DIR / "logs" / f"{Path(__file__).stem}.log")
-):
-    file_handler = logging.handlers.RotatingFileHandler(filename)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-    return file_handler
-
-
-def get_stream_handler(formatter):
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setLevel(logging.DEBUG)
-    stream_handler.setFormatter(formatter)
-    return stream_handler
-
-
-formatter = CustomJSONFormatter("%(asctime)s")
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(get_stream_handler(formatter))
-logger.addHandler(get_file_handler(formatter))
+formatter = app_logger.CustomJSONFormatter("%(asctime)s")
+logger = app_logger.get_logger(
+    __name__, formatter, fileout=(ROOT_DIR / "logs" / f"{Path(__file__).stem}.log")
+)
+configure_logger(logger)
 
 
 async def get_extra_info(request: Request, response: Response):
     return {
         "req": {
             "url": request.url.path,
-            "headers": dict(
-                request.headers
-            ),  # request.headers is supposed to be a mapping that handles duplicate keys so coercing to dict is prone to break but ¯\_(ツ)_/¯
+            # request.headers is supposed to be a mapping that handles duplicate keys
+            # so coercing to dict is prone to break but ¯\_(ツ)_/¯
+            "headers": dict(request.headers),
             "method": request.method,
             "http_version": request.scope["http_version"],
             "original_url": request.url.path,
@@ -189,8 +107,10 @@ async def get_extra_info(request: Request, response: Response):
 
 
 async def write_log_data(request, response):
-    # From the docs: The fourth keyword argument is extra which can be used to pass a dictionary
-    # which is used to populate the __dict__ of the LogRecord created for the logging event with user-defined attributes.
+    # From the docs:
+    # The fourth keyword argument is extra which can be used to pass a dictionary
+    # which is used to populate the __dict__ of the LogRecord
+    # created for the logging event with user-defined attributes.
     logger.info(
         request.method + " " + request.url.path,
         extra={"extra_info": await get_extra_info(request, response)},
@@ -210,23 +130,14 @@ async def log_request(request: Request, call_next):
     return response
 
 
-########################################################################
-
-
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    # List the HTML files in the title pages directory
     files = [
         f.name for f in DOWNLOADED_TITLEPAGES_DIR.iterdir() if f.name.endswith(".html")
     ]
-
-    # Render the template with the list of files
     return templates.TemplateResponse(
         "index.html", {"request": request, "files": files}
     )
-
-
-engine = create_engine(DATABASE_URL, echo=True)
 
 
 def get_session():
@@ -234,7 +145,13 @@ def get_session():
         yield session
 
 
-SessionDep = Annotated[Session, Depends(get_session)]
+DatabaseSessionDep = Annotated[Session, Depends(get_session)]
+
+
+class TitlesPostedResponse(BaseModel):
+    job_id: str
+    payload_sent: list[int]
+    actual_payload_to_submit: list[int]
 
 
 class TitleResponse(BaseModel):
@@ -261,7 +178,7 @@ class TitleResponseDecoder(json.JSONEncoder):
 
 
 @app.get("/api/title/{title_id}", response_model=Dict[int, TitleResponse])
-def get_title(title_id: int, session: SessionDep):
+def get_title(title_id: int, session: DatabaseSessionDep):
     title = session.exec(
         select(
             Title.id,
@@ -287,7 +204,8 @@ def get_title(title_id: int, session: SessionDep):
 
 @app.get("/api/titles", response_model=Dict[int, TitleResponse])
 def get_all_available_titles(
-    session: SessionDep, available_in: Annotated[list[str] | None, Query()] = ("US",)
+    session: DatabaseSessionDep,
+    available_in: Annotated[list[str] | None, Query()] = ("US",),
 ):
     titles = session.exec(
         select(
@@ -345,7 +263,7 @@ def get_all_available_titles(
     return response_obj
 
 
-@app.post("/api/titles")
+@app.post("/api/titles", response_model=TitlesPostedResponse)
 async def store_title_ids_for_processing(payload: list[int]):
     job_id = str(uuid4())
     global_job_store[job_id] = payload
@@ -356,43 +274,10 @@ async def store_title_ids_for_processing(payload: list[int]):
     }
 
 
-def find_all_script_elements(html: HTMLContent):
-    """
-    Finds and returns all <script> elements in the provided HTML string.
-
-    :param html: A string containing the HTML content.
-    :return: A list of dictionaries with script details (content and attributes).
-    """
-    soup = BeautifulSoup(html, "html.parser")
-    script_elements = soup.find_all("script")
-
-    scripts_info = []
-    for script in script_elements:
-        script_info = {
-            "content": script.string,  # Inline script content (None if external)
-            "attributes": script.attrs,  # Script element attributes (e.g., src, type)
-        }
-        scripts_info.append(script_info)
-
-    return scripts_info
-
-
-def _sanitize_pythonmonkey_obj(obj):
-    if isinstance(obj, dict):
-        return {k: _sanitize_pythonmonkey_obj(v) for k, v in obj.items()}
-    elif isinstance(obj, list):
-        return [_sanitize_pythonmonkey_obj(v) for v in obj]
-    elif obj == pm.null:
-        return None
-    else:
-        if isinstance(obj, float):
-            if obj.is_integer():
-                return int(obj)
-        return obj
-
-
 async def fetch_and_process_title(
-    title_id: int, session_handler: SessionHandler, background_tasks: BackgroundTasks
+    title_id: int,
+    session_handler: HttpSessionHandler,
+    background_tasks: BackgroundTasks,
 ) -> list[dict]:
     request_url = f"https://www.netflix.com/title/{title_id}"
     async with session_handler.limiter:
@@ -402,28 +287,17 @@ async def fetch_and_process_title(
                 response.raise_for_status()
 
             html_content = HTMLContent(await response.text())
-            scripts = find_all_script_elements(html_content)
 
             background_tasks.add_task(
                 save_response_body,
                 html_content,
-                f"/Users/asibalo/Documents/Dev/PetProjects/netflix_critic_data/data/raw/title/{title_id}.html",
+                DOWNLOADED_TITLEPAGES_DIR / f"{title_id}.html",
             )
 
-            for script in scripts:
-                content = script["content"]
-                if content is None:
-                    continue
-                context_def = content.find("reactContext =")
-                if context_def != -1:
-                    try:
-                        react_context = script["content"][context_def:]
-                        return _sanitize_pythonmonkey_obj(
-                            pm.eval(react_context).models.nmTitleUI.data.sectionData
-                        )
-                    except (KeyError, AttributeError, pm.SpiderMonkeyError) as e:
-                        logger.exception(e)
-                        return []
+            try:
+                return extract_netflix_react_context(html_content)
+            except ContextExtractionError:
+                return []
 
 
 async def scrape_serp_for_ratings(
@@ -444,14 +318,14 @@ async def scrape_serp_for_ratings(
         background_tasks.add_task(
             save_response_body,
             html_content,
-            f"/Users/asibalo/Documents/Dev/PetProjects/netflix_critic_data/data/raw/serp/{netflix_id}.html",
+            DOWNLOADED_SERP_PAGES_DIR / f"{netflix_id}.html",
         )
         return await get_ratings(netflix_id, html_content)
 
 
 async def download_title_and_lookup_ratings(
     title_id,
-    http_session_handler: SessionHandler,
+    http_session_handler: HttpSessionHandler,
     background_tasks: BackgroundTasks,
 ) -> dict[str, Any]:
     title_data = await fetch_and_process_title(
@@ -470,7 +344,7 @@ async def download_title_and_lookup_ratings(
 
 async def stream_ratings(job_id: str, db_session, background_tasks: BackgroundTasks):
     tasks = []
-    http_session_handler = SessionHandler()
+    http_session_handler = HttpSessionHandler()
 
     for title_id in global_job_store[job_id]:
         task = asyncio.create_task(
@@ -555,7 +429,7 @@ async def stream_ratings(job_id: str, db_session, background_tasks: BackgroundTa
 
 @app.get("/api/stream/{job_id}", response_model=Dict[int, TitleResponse])
 async def stream_data(
-    job_id: str, db_session: SessionDep, background_tasks: BackgroundTasks
+    job_id: str, db_session: DatabaseSessionDep, background_tasks: BackgroundTasks
 ):
     return StreamingResponse(
         stream_ratings(job_id, db_session, background_tasks),
