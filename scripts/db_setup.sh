f2a055d07380bc4c278e61878688d434ad1833f4
adosib <adosib1@gmail.com>
2025-01-23 18:54:56 -0600

    Revert "Add pgdump file so that the container actually has something to load :facepalm:"
    This reverts commit b3287774ba454d88ac264a4a42fdb70abc1b1153.

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "\n************** Restoring database from file at /data/pg_dump/Ft/pgdump... \n"
-pg_restore -d postgres /data/pg_dump/Ft/pgdump
+echo "\n************** Restoring database from file at /data/pg_dump/Fc/pg.dump... \n"
+pg_restore -d postgres /data/pg_dump/Fc/pg.dump
